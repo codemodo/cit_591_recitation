35f3ec5c4f4267f4bfa9b849e68eba0deb55cba7
Alex Fiuk <afiuk@seas.upenn.edu>
2015-11-06 13:49:03 -0500

    added methods

--- Name.java
+++ Name.java
@@ -15,14 +15,6 @@ public class Name {
 
 	public Name(String title, String first, String middle, String surname, String paternal, String maternal,
 			String generational, String professional) {
-		this.title = title;
-		this.first = first;
-		this.middle = middle;
-		this.surname = surname;
-		this.paternal = paternal;
-		this.maternal = maternal;
-		this.suffixGenerational = generational;
-		this.suffixProfessional = professional;
 	}
 
 	public String getTitle() {
@@ -59,5 +51,70 @@ public class Name {
 
 	public String getNameAllCaps() {
 		return null;
+	}
+
+	public void setFirst() {
+	}
+
+	public void setTitle() {
+	}
+
+	public void setMiddle() {
+	}
+
+	public void setSurname() {
+	}
+
+	public void setPaternal() {
+	}
+
+	public void setMaternal() {
+	}
+
+	public void setSuffixGenerational() {
+	}
+
+	public void setSuffixProfessional() {
+	}
+	
+	public boolean isPalindrome() {
+		return false;	
+	}
+	
+	/*
+	 * Return a string that combines the first middle and last names.
+	 * e.g., Jim Paper Halpert
+	 */
+	public String concatFirstMiddleLast() {
+		return null;
+	}
+	
+	/*
+	 * Come up with your own meaningful string representation of this Name.
+	 */
+	@Override
+	public String toString() {
+		return null;
+	}
+	
+	/*
+	 * Checks whether first name matches any of your TAs' first name.
+	 */
+	public boolean matchesTAsFirstName() {
+		return false;
+	}
+	
+	/*
+	 * Checks if input string matches first name
+	 */
+	public boolean matchesFirstName(String s) {
+		return false;
+	}
+	
+	/*
+	 * Checks whether the two surnames match
+	 */
+	public boolean isRelativeOf(Name name) {
+		return false;
 	}
 }
