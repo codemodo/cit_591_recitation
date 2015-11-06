5a61b94a99282dccb9ecb9746ae234f6d4d9679e
Zhiyuan Li <zhil@seas.upenn.edu>
2015-11-06 16:05:46 -0500

    update setfirst mothod

--- Name.java
+++ Name.java
@@ -53,7 +53,11 @@ public class Name {
 		return null;
 	}
 
-	public void setFirst() {
+	/**
+	*@Zhiyuan Li
+	*/
+	public void setFirst(String first) {
+		this.first = first;
 	}
 
 	public void setTitle() {
