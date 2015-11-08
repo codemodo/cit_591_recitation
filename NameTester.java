65ea27f13ab1dffab7f0e2614e651eca9ce3b872
ron <redde@seas.upenn.edu>
2015-11-07 21:39:18 -0500

    ron test

--- NameTester.java
+++ NameTester.java
@@ -5,7 +5,7 @@
 public class NameTester {
 	
 	public static void main(String[] args) {
-		Name name = new Name("Mr.", "Bob", "Manfred", "Jinkens", "O'Brien", "Jinkens", "III", "Ph.D.");
+		Name name = new Name("Mr.", "Bob", "Manfred", "Jinkens", "Ron", "O'Brien", "Jinkens", "III", "Ph.D.");
 
 		System.out.printf("%s %s %s %s (formerly %s), %s %s, %s\n", name.getTitle(), name.getFirst(), name.getMiddle(),
 				name.getSurname(), name.getMaternal(), name.getPaternal(), name.getSuffixGenerational(),
